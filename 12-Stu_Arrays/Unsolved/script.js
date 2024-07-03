43243ea8ce1cf330207fcaa9136a6f8a224aa94a
Justin Kao <[justinkao010@gmail.com]>
2024-07-03 11:21:49 -0700

    solved 12-16 student activities

--- 12-Stu_Arrays/Unsolved/script.js
+++ 12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,14 @@
 // WRITE YOUR CODE HERE
+const studentsName = ["Bob", "Billy", "Joe"];
+
+console.log(studentsName.length);
+
+console.log(`Welcome to the class, ${studentsName[0]}`);
+console.log(`Welcome to the class, ${studentsName[1]}`);
+console.log(`Welcome to the class, ${studentsName[2]}`);
+
+studentsName[0] = "Justin";
+
+if (studentsName[0] !== "Bob") {
+    console.log(`${studentsName[0]} is in the class`);
+}
--- 14-Stu_Iteration/Unsolved/script.js
+++ 14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,12 @@
 // WRITE YOUR CODE BELOW
+const students = ["Bob", "Billy", "Joe", "Justin", "Chris"];
+
+console.log(students.length);
+
+for (let i = 0; i < students.length; i++) {
+    console.log(`Great to see you, ${students[i]}!`);
+}
+
+for (let name of students) {
+    console.log(`Great to see you, ${name}!`);
+}
